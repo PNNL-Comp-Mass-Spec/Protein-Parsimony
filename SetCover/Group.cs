923af1ab68eff8fa078015b96ba622cea7d6ca85
Joshua Aldrich <Joshua.Aldrich@pnnl.gov>
2013-10-01 22:34:29 +0000

    Set the groupNodes names appropriately.

--- SetCover/Group.cs
+++ SetCover/Group.cs
@@ -14,6 +14,12 @@ namespace SetCover
         {
             this.nodeGroup = groupedNodes;
             children = groupedNodes[0].children;
+            this.nodeNames = new string[groupedNodes.Count];
+            for (int i = 0; i < nodeNames.Length; i++)
+            {
+                this.nodeNames[i] = groupedNodes[i].nodeName;
+            }
+            this.nodeName = String.Join("-", nodeNames);
 
             foreach (Node child in children)
             {
@@ -22,7 +28,6 @@ namespace SetCover
                     child.children.Remove(grNode);
                 }
             }
-
             foreach (Node child in children)
             {
                 child.children.Add(this);
