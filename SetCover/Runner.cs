d1fb84e9692fb513d4912cd0fd14dc07367b173b
Joshua Aldrich <Joshua.Aldrich@pnnl.gov>
2013-10-01 00:45:42 +0000

    remove the datatable after its use to free up memory

--- SetCover/Runner.cs
+++ SetCover/Runner.cs
@@ -12,11 +12,11 @@ namespace SetCover
         public bool RunAlgorithm(string filename)
         {
             DataTable dt = Utilities.TextFileToDataTableAssignTypeString(filename, false);
-
             NodeBuilder nb = new NodeBuilder();
             List<Node> Peptides = new List<Node>();
             List<Node> Proteins = new List<Node>();
             nb.RunAlgorithm(dt, ref Proteins, ref Peptides);
+            dt = null;
             NodeCollapser nc = new NodeCollapser();
             nc.RunAlgorithm(ref Proteins, ref Peptides);
             DFS dfs = new DFS();
