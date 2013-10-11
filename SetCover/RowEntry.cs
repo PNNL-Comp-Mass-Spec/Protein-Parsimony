0914447872a6cd28e7e661bc9da797460580b8f9
Joshua Aldrich <Joshua.Aldrich@pnnl.gov>
2013-10-11 23:31:12 +0000

    Adding the rowentry structure.

---
+++ SetCover/RowEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SetCover
+{
+    public struct RowEntry
+    {
+        public string ProteinEntry;
+        public string PeptideEntry;
+    }
+}
