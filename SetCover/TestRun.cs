9636d2c94187d4c67b7f0d6c22be64edb06f13e1
alchemistmatt <matthew.monroe@pnnl.gov>
2018-01-17 19:04:56 -0800

    Add unit tests

--- SetCover/TestRun.cs
+++ SetCover/TestRun.cs
@@ -10,16 +10,19 @@ namespace SetCover
     class TestRun
     {
         [Test]
-        public void TestFile(string[] args)
+        public void TestFile()
         {
-            var file = @"C:\Users\aldr699\Documents\Visual Studio 2010\Projects2\ProteinPars\SetCover\TestData\T_Row_Metadata.txt";
+            var file = @"\\proto-2\UnitTest_Files\ProteinParsimony\T_Row_Metadata.txt";
+
+            Runner.GetDefaultOutputFileNames(file, out var parsimonyResultsFilePath, out var proteinGroupMembersFilePath);
+
             var run = new Runner();
-            run.RunAlgorithm(file,"");
+            run.RunGUIAlgorithm(file, parsimonyResultsFilePath, proteinGroupMembersFilePath);
 
         }
 
         [Test]
-        public void TestNodeBuilder()
+        public void TestNodeBuilderA()
         {
             var peptideProteinMapList = new List<RowEntry>
             {
@@ -33,6 +36,71 @@ namespace SetCover
                 new RowEntry {ProteinEntry = "prot_D", PeptideEntry = "pep_D"},
                 new RowEntry {ProteinEntry = "prot_D", PeptideEntry = "pep_E"}
             };
+
+            var expectedOutput = new List<string> {
+                "Protein\tPeptide",
+                "prot_C; prot_B\tpep_A",
+                "prot_C; prot_B\tpep_C",
+                "prot_C; prot_B\tpep_B",
+                "prot_D\tpep_E",
+                "prot_D\tpep_D"};
+
+            TestNodeBuilder(peptideProteinMapList, expectedOutput);
+
+        }
+
+        [Test]
+        public void TestNodeBuilderB()
+        {
+            var peptideProteinMapList = new List<RowEntry>
+            {
+                new RowEntry {ProteinEntry = "prot_A", PeptideEntry = "pep_A"},
+                new RowEntry {ProteinEntry = "prot_B", PeptideEntry = "pep_A"},
+                new RowEntry {ProteinEntry = "prot_B", PeptideEntry = "pep_B"},
+                new RowEntry {ProteinEntry = "prot_B", PeptideEntry = "pep_C"},
+                new RowEntry {ProteinEntry = "prot_C", PeptideEntry = "pep_A"},
+                new RowEntry {ProteinEntry = "prot_C", PeptideEntry = "pep_B"},
+                new RowEntry {ProteinEntry = "prot_C", PeptideEntry = "pep_C"},
+                new RowEntry {ProteinEntry = "prot_D", PeptideEntry = "pep_D"},
+                new RowEntry {ProteinEntry = "prot_D", PeptideEntry = "pep_E"},
+                new RowEntry {ProteinEntry = "prot_E", PeptideEntry = "pep_F"},
+                new RowEntry {ProteinEntry = "prot_E", PeptideEntry = "pep_G"},
+                new RowEntry {ProteinEntry = "prot_E", PeptideEntry = "pep_H"},
+                new RowEntry {ProteinEntry = "prot_E", PeptideEntry = "pep_I"},
+                new RowEntry {ProteinEntry = "prot_E", PeptideEntry = "pep_K"},
+                new RowEntry {ProteinEntry = "prot_F", PeptideEntry = "pep_F"},
+                new RowEntry {ProteinEntry = "prot_G", PeptideEntry = "pep_I"},
+                new RowEntry {ProteinEntry = "prot_G", PeptideEntry = "pep_G"},
+                new RowEntry {ProteinEntry = "prot_H", PeptideEntry = "pep_F"},
+                new RowEntry {ProteinEntry = "prot_H", PeptideEntry = "pep_G"},
+                new RowEntry {ProteinEntry = "prot_I", PeptideEntry = "pep_H"},
+                new RowEntry {ProteinEntry = "prot_I", PeptideEntry = "pep_J"},
+                new RowEntry {ProteinEntry = "prot_J", PeptideEntry = "pep_J"},
+                new RowEntry {ProteinEntry = "prot_K", PeptideEntry = "pep_L"},
+                new RowEntry {ProteinEntry = "prot_L", PeptideEntry = "pep_M"},
+            };
+
+            var expectedOutput = new List<string> {
+                "Protein\tPeptide",
+                "prot_C; prot_B\tpep_A",
+                "prot_C; prot_B\tpep_C",
+                "prot_C; prot_B\tpep_B",
+                "prot_D\tpep_E",
+                "prot_D\tpep_D",
+                "prot_E\tpep_K",
+                "prot_E\tpep_H",
+                "prot_E\tpep_I",
+                "prot_E\tpep_F",
+                "prot_E\tpep_G",
+                "prot_K\tpep_L",
+                "prot_L\tpep_M"
+            };
+
+            TestNodeBuilder(peptideProteinMapList, expectedOutput);
+
+        }
+        private void TestNodeBuilder(List<RowEntry> peptideProteinMapList, IReadOnlyList<string> expectedOutput)
+        {
 
             var nodebuilder = new NodeBuilder();
             var nodecollapser = new NodeCollapser();
@@ -56,24 +124,33 @@ namespace SetCover
                 Console.WriteLine(item);
             }
 
-            //Expected output:
-            //prot_C-prot_B	pep_A
-            //prot_C-prot_B	pep_B
-            //prot_C-prot_B	pep_C
-            //prot_D	pep_E
-            //prot_D	pep_D
+            if (expectedOutput.Count == 0)
+                return;
 
+            for (var i = 0; i < outLines.Count; i++)
+            {
+                if (i >= expectedOutput.Count)
+                {
+                    Assert.Fail("Extra, unexpected output line: " + outLines[i]);
+                }
 
+                Assert.AreEqual(expectedOutput[i], outLines[i], "Mismatch on line {0}", i + 1);
+            }
+
+            if (expectedOutput.Count > outLines.Count)
+            {
+                Assert.Fail("Output did not include additional, expected lines, starting with " + expectedOutput[outLines.Count]);
+            }
         }
 
         [Test]
         public void TestResultDB()
         {
-            var datadbfolder = @"C:\Users\aldr699\Documents\Visual Studio 2010\Projects2\ProteinPars\SetCover\TestData";
+            var datadbfolder = @"\\proto-2\UnitTest_Files\ProteinParsimony";
             var datafile = "Results.db3";
 
             var runner = new Runner();
-            runner.RunAlgorithm(datadbfolder,datafile);
+            runner.RunAlgorithm(datadbfolder, datafile, "T_Row_Metadata");
 
 
         }
